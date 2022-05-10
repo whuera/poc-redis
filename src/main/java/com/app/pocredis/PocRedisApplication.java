c101461f441d986915019b6ee2e9c1c548fb6439
William Huera <whuera@gmail.com>
2022-05-10 09:51:53 -0500

    first commit

---
+++ src/main/java/com/app/pocredis/PocRedisApplication.java
@@ -0,0 +1,15 @@
+package com.app.pocredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public
+class PocRedisApplication {
+
+    public static
+    void main (String[] args) {
+        SpringApplication.run ( PocRedisApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/app/pocredis/PocRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pocredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocRedisApplicationTests {
+
+    @Test
+    void contextLoads ( ) {
+    }
+
+}
